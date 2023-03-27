14d1b72f29fb0e265bccc277b4f30b6261c52228
TimothyKagondu <timothy.2k21@gmail.com>
2023-03-27 17:45:39 +0300

    adding guide lines

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,7 @@
 class HeroPower < ApplicationRecord
+    
+# `strength` must be one of the following values: 'Strong', 'Weak', 'Average'
+
     validates :strength, inclusion: {in: %w(Strong Weak Average)} 
 
     belongs_to :hero

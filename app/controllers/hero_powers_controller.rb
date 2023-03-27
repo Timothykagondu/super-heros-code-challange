80d301c0d828428606f4488040eb389cb649fa32
TimothyKagondu <timothy.2k21@gmail.com>
2023-03-27 17:22:12 +0300

    adding code to controllers

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,23 @@
 class HeroPowersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    skip_before_action :verify_authenticity_token
+    
+def create
+    hero =Hero.find(params[:hero_id])
+    hero_power = HeroPower.create!(hero_power_params) 
+    render json: hero, serializer:
+    SingleheroSerializer
+
+end
+
+private
+
+def hero_power_params
+    params.permit(:strength, :hero_id, :power_id) 
+  end
+
+  def render_not_found_response
+    render json: { error: "Validation errors" }, status: :not_found
+ end
+
 end
